43a3b04690b76070278f4775dff07d9fbb707917
KALAISELVAN-18 <kalai2808200@gmail.com>
2026-03-06 22:28:08 +0530

    footer file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Upload from './pages/Upload';
 import Scan from './pages/Scan';
@@ -27,6 +28,7 @@ function App() {
             </Routes>
           </div>
         </main>
+        <Footer />
       </div>
     </Router>
   );
